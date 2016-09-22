ccf837b38d22ff67b1115e68c2bdc77b57380099
ak970101 <abu7meed97@hotmail.com>
2016-09-22 11:16:44 +0200

    Ändringar i MainActivity Ny aktivetet TestActivity

--- app/src/main/java/com/example/ak970101/layoutilayout/testAddActivity.java
+++ app/src/main/java/com/example/ak970101/layoutilayout/testAddActivity.java
@@ -3,16 +3,11 @@ package com.example.ak970101.layoutilayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class testAddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void nyMetod(){
-        int i = 1+1;
-        int ii=i+1;
-        int iii=ii+1;
+        setContentView(R.layout.activity_test_add);
     }
 }
